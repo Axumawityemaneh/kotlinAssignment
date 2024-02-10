9dd4bcbc4401af362ebd698ef6ec7e47ba22aae9
axumawityemaneh <yemanehaxumawit@gmail.com>
2024-02-10 18:38:02 +0300

    add a file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var fullName= "AXUMAWIT"
+    println(fullName)
+    var age= "24"
+    println(age)
+    var phoneNumber= "+254115622662"
+    println(phoneNumber)
+    var wight= "59"
+    println(wight)
+    val citizen= "true"
+println(citizen)
+val nonCitizen= "false"
+    println(nonCitizen)
+}
